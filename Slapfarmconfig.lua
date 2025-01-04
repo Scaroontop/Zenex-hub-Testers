ad468e83f6dea34407f6c38a9867cce2cb92f37e
Scaroontop <162757246+Scaroontop@users.noreply.github.com>
2025-01-04 18:44:25 -0500

    Omg 1000+ features

---
+++ Slapfarmconfig.lua
@@ -0,0 +1,35 @@
+AutoExecute = false -- If true, the script will auto-execute himself without placing it in the auto-execute folder. (YOUR EXECUTOR MUST SUPPORT QUEUE_ON_TELEPORT)
+
+BypassAnticheat = true -- Bypass the anti-cheat enabled so no issues happen with the Anti-cheat.
+
+GhostInvis = true -- Applying Invisibility To Player (666 Slaps required)
+
+Nocollide = false -- don't set it to true if you don't wanna problems.
+
+AntiRecord = true -- It puts common English words and kicks if someone puts the keyword.
+
+Safespot = true -- It takes you to a safe spot and then starts the farm.
+
+Ballerfarm = false -- Gives you barely any slaps 2-10 extra slaps it bad.
+
+Blinkfarm = false -- This gives a lot of slaps with replica Disabled since people will most likely not have Blink.
+
+Replicafarm = true -- spawns a replica for the farm.		
+
+SlappowerV1 = true -- It does a normal amount of power hitting the replica blink or baller (Recommended for bad devices)
+
+SlappowerV1.5 = false -- It does more power but causes more lag and in return, you get more slaps (Recommended for a Decent device)
+
+SlappowerV2 = false -- It does a lot of power Use V1 for less lag but this does give more slaps
+
+FarmOrbs = true -- Farms every orb in-game
+
+FarmToolbox = true -- Farm toolbox (very useless)
+
+SlapplefarmV1 = true -- Farms All Slapples
+
+SlapplefarmV2 = true -- Keep this on to keep grabbing the slapples just in case you don't fully grab them. (This could cause issues just use v1)
+
+FastServerhop = false -- Serverhops fast (This could cause issues)
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Scaroontop/Zenex-hub-Testers/refs/heads/main/Slap%20farm.lua'))()
