5e3ebb6b261b1c555dc7d9c7c40a4133730e3453
Scaroontop <162757246+Scaroontop@users.noreply.github.com>
2025-01-05 13:11:10 -0500

    Added Latest config

--- Slapfarmconfig.lua
+++ Slapfarmconfig.lua
@@ -2,13 +2,13 @@ AutoExecute = false -- If true, the script will auto-execute himself without pla
 
 BypassAnticheat = true -- Bypass the anti-cheat enabled so no issues happen with the Anti-cheat.
 
-GhostInvis = true -- Applying Invisibility To Player (666 Slaps required)
+AntiRecord = true -- It puts common English words and kicks if someone puts the keyword.
 
 Nocollide = false -- don't set it to true if you don't wanna problems.
 
-AntiRecord = true -- It puts common English words and kicks if someone puts the keyword.
+GhostInvis = true -- Applying Invisibility To Player (666 Slaps required)
 
-Safespot = true -- It takes you to a safe spot and then starts the farm.
+Safespot = true -- It takes you to a safe spot and then starts the farm
 
 Ballerfarm = false -- Gives you barely any slaps 2-10 extra slaps it bad.
 
@@ -16,20 +16,24 @@ Blinkfarm = false -- This gives a lot of slaps with replica Disabled since peopl
 
 Replicafarm = true -- spawns a replica for the farm.		
 
-SlappowerV1 = true -- It does a normal amount of power hitting the replica blink or baller (Recommended for bad devices)
+SlappowerMethod1 = true -- It does a normal amount of power hitting the replica blink or baller (Recommended for bad devices)
 
-SlappowerV1.5 = false -- It does more power but causes more lag and in return, you get more slaps (Recommended for a Decent device)
+SlappowerMethod2 = false -- It does more power but causes more lag and in return, you get more slaps (Recommended for a Decent device)
 
-SlappowerV2 = false -- It does a lot of power Use V1 for less lag but this does give more slaps
+SlappowerMethod3 = false -- It does a lot of power and this does give more slaps (Recommened on a good device)
+
+SlappowerMethod4 = false -- THis will lag so much that it will prob crash roblox you do get a lot of slaps from this though
+
+SlapplefarmV1 = true -- Farms All Slapples
+
+SlapplefarmV2 = true -- Keep this on to keep grabbing the slapples just in case you don't fully grab them.
 
 FarmOrbs = true -- Farms every orb in-game
 
 FarmToolbox = true -- Farm toolbox (very useless)
 
-SlapplefarmV1 = true -- Farms All Slapples
+Refarm = false -- It farms the server again and again 
 
-SlapplefarmV2 = true -- Keep this on to keep grabbing the slapples just in case you don't fully grab them. (This could cause issues just use v1)
-
-FastServerhop = false -- Serverhops fast (This could cause issues)
+Serverhop = true -- This serverhops if you don't want to serverhop use Refarm
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Scaroontop/Zenex-hub-Testers/refs/heads/main/Slap%20farm.lua'))()
